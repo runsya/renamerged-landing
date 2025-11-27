a097d88fe23be8627f65d55c8222bb966efe76c6
iunoo2 <syahbandi.bbn@gmail.com>
2025-11-27 11:06:19 +0700

    Updated ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'renamerged-fe',
-      script: 'npm',
-      args: 'run dev',
-      cwd: '/home/iuno/.pm2/logs',
-      env: {
-        NODE_ENV: 'development'
-      }
-    },
-    {
-      name: 'renamerged-be',
+      name: 'renamerged',
       script: 'server/index.js',
       cwd: '/home/iuno/.pm2/logs',
       env: {
